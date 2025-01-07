7da49e9095791f53ee064f4776beb0e0888c6063
Ankit Panda <ankitpanda6981@gmail.com>
2025-01-08 00:50:08 +0530

    Implemented auth-call back logic and setup prisma,neon db

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,15 +1,48 @@
 "use client"
-import { trpc } from "../_trpc/client"
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
+import { useEffect } from 'react'
 
 const Page = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const origin = searchParams.get('origin')
 
-    const user =  trpc.getUser.useQuery()
+  const { data , error } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  })
 
-    return (
-        <div>
-            <p>{JSON.stringify(user.data)}</p>
-        </div>
-    )
+  useEffect(() => {
+    if (data) {
+        const { success } = data
+        if (success) {
+            // user is synced to db
+            //console.log('Data fetched successfully:', data);
+            router.push(origin ? `/${origin}` : '/dashboard')
+        }
+    }
+    else if (error) {
+        if (error.data?.code === 'UNAUTHORIZED') {
+            router.push('/sign-in')
+        }
+    }
+
+  }, [data, origin,router, error]);
+
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>
+          Setting up your account...
+        </h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  )
 }
 
 export default Page
