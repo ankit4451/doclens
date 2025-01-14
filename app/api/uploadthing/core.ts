1f33bac93633d753af09374a7cc32a6419d6cf24
Ankit Panda <ankitpanda6981@gmail.com>
2025-01-15 00:18:18 +0530

    Setup Pinecone and OpenAI environment

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,56 +1,99 @@
-import { db } from '@/db'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import {
-  createUploadthing,
-  type FileRouter,
-} from 'uploadthing/next'
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
+import { pineconeClient } from "@/lib/pinecone";
 
-const f = createUploadthing()
+const f = createUploadthing();
 
 const middleware = async () => {
-  const { getUser } = getKindeServerSession()
-  const user = await getUser()
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-  if (!user || !user.id) throw new Error('Unauthorized')
+  if (!user || !user.id) throw new Error("Unauthorized");
 
-  return { userId: user.id }
-}
+  return { userId: user.id };
+};
 
 const onUploadComplete = async ({
   metadata,
   file,
 }: {
-  metadata: Awaited<ReturnType<typeof middleware>>
+  metadata: Awaited<ReturnType<typeof middleware>>;
   file: {
-    key: string
-    name: string
-    url: string
-  }
+    key: string;
+    name: string;
+    url: string;
+  };
 }) => {
   const isFileExist = await db.file.findFirst({
     where: {
       key: file.key,
     },
-  })
+  });
 
-  if (isFileExist) return
+  if (isFileExist) return;
 
-  await db.file.create({
+  const createdFile = await db.file.create({
     data: {
       key: file.key,
       name: file.name,
       userId: metadata.userId,
       url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-      uploadStatus: 'PROCESSING',
+      uploadStatus: "PROCESSING",
     },
-  })
+  });
 
-}
+  try {
+    const response = await fetch(
+      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+    );
+
+    const blob = await response.blob();
+
+    const loader = new PDFLoader(blob);
+
+    const pageLevelDocs = await loader.load();
+
+    //const pagesAmt = pageLevelDocs.length;
+    // vectorize and index entire document
+    const pineconeIndex = pineconeClient.Index("doculens");
+
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
+
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+      namespace: createdFile.id,
+    });
+
+    await db.file.update({
+      data: {
+        uploadStatus: "SUCCESS",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  } catch {
+    await db.file.update({
+      data: {
+        uploadStatus: "FAILED",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  }
+};
 
 export const ourFileRouter = {
-  pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     .middleware(middleware)
     .onUploadComplete(onUploadComplete),
-} satisfies FileRouter
+} satisfies FileRouter;
 
-export type OurFileRouter = typeof ourFileRouter
+export type OurFileRouter = typeof ourFileRouter;
---
+++ lib/pinecone.ts
@@ -0,0 +1,3 @@
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+
+export const pineconeClient = new PineconeClient();
