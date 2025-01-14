f52783f9c46d3c63a8953b297b157a248392f81e
Ankit Panda <ankitpanda6981@gmail.com>
2025-01-14 21:37:01 +0530

    Added backend API for messages post req

---
+++ app/api/message/route.ts
@@ -0,0 +1,39 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+import { db } from "@/db";
+
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+
+export const POST = async (req: NextRequest) => {
+  // endpoint for asking a question to a pdf file
+
+  const body = await req.json();
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const { id: userId } = user;
+
+  if (!userId) return new Response("Unauthorized", { status: 401 });
+
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId,
+    },
+  });
+
+  if (!file) return new Response("Not found", { status: 404 });
+
+  await db.message.create({
+    data: {
+      text: message,
+      isUserMessage: true,
+      userId,
+      fileId,
+    },
+  });
+};
